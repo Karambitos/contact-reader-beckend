f463b348770779df10f62f3fd0ea40f5fd7b0e64
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2024-02-19 16:30:31 +0100

    move DB_HOST to config

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose/index.js";
 import { app } from "./app.js";
-
-const DB_HOST = "mongodb+srv://Admin:^zMqPmtDN9V8HWS1yn@cluster0.egbthzb.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+import { DB_HOST } from "./config.js";
 
 mongoose.connect(DB_HOST)
     .then(() => {
         console.log("Database connection successful")
         app.listen(3000, () => {
-            console.log("Server is running. Use our API on port: 3001");
+            console.log("Server is running. Use our API on port: 3000");
         });
     })
     .catch(error => {
