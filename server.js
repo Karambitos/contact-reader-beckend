5187d8dbf3dddfa8bb09be0f69e544cdcce4cd88
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2024-02-19 17:13:52 +0100

    move DB_HOST to .env

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose/index.js";
 import { app } from "./app.js";
-import dotenv from 'dotenv';
 
-dotenv.config();
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.connect(DB_HOST)
     .then(() => {
         console.log("Database connection successful")
-        app.listen(3000, () => {
-            console.log("Server is running. Use our API on port: 3000");
+        app.listen(PORT, () => {
+            console.log(`Server is running. Use our API on port: ${PORT}`);
         });
     })
     .catch(error => {
